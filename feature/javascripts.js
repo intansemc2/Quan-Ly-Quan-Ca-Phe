6656104c1a29c931d826d772490a8851ba5dfd3c
intansemc2 <intansemc2@gmail.com>
2020-05-15 07:38:56 +0700

    Remove unnesscessary feature folder

--- feature/javascripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#example').DataTable({
-        "paging":   true,
-        "ordering": true,
-        "info":     true,
-
-        "order": [[ 0, "asc" ]]
-    });
-});

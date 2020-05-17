4495ec2d0e5884404bef766932768a937c164b65
hd789789789 <lenguyenhaiduy789789@gmail.com>
2020-05-17 15:15:25 +0700

    Add new project

---
+++ Website/js/add-cus.js
@@ -0,0 +1,39 @@
+function hien(){
+	document.getElementById("add-cus").style.display = 'inline';
+}
+
+function an(){
+	document.getElementById("add-cus").style.display = 'none';
+}
+
+function hien2(){
+	document.getElementById("upl-cus").style.display = 'inline';
+}
+
+function an2(){
+	document.getElementById("upl-cus").style.display = 'none';
+}
+
+function hien3(){
+	document.getElementById("add-food").style.display = 'inline';
+}
+
+function an3(){
+	document.getElementById("add-food").style.display = 'none';
+}
+
+function hien4(){
+	document.getElementById("upl-food").style.display = 'inline';
+}
+
+function an4(){
+	document.getElementById("upl-food").style.display = 'none';
+}
+
+function hien5(){
+	document.getElementById("add-table").style.display = 'inline';
+}
+
+function an5(){
+	document.getElementById("add-table").style.display = 'none';
+}

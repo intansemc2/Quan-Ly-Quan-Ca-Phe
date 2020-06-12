6d59da9f3dc890744e6931127690f19fd28b071d
intansemc2 <intansemc2@gmail.com>
2020-06-13 05:50:20 +0700

    Add new GUIs

--- AdminPage-ASP/quanlyquancaphe/wwwroot/js/custom-alert.js
+++
@@ -1,3 +0,0 @@
-let createAlerts = (alertType, content) => {
-    return `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert"><span>${content}</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
-};
